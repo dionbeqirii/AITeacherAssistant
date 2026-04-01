79f2fa799b90e92194ad735aba945885d864e3b1
Dionbeqirii <dbeqiri688@gmail.com>
2026-04-01 15:37:13 +0200

    Fix: next.config hostname and distDir

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: '.next', // I ndihmon Vercel-it të gjejë folderin e gjeneruar
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://mguorzqsfxgzpxgihhfz.supabase.co', // Zëvendësoje me ID-në e projektit tënd
+        hostname: 'mguorzqsfxgzpxgihhfz.supabase.co', // Hoqa https:// nga këtu
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
